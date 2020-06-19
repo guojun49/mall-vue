02a211da67b7ad1cf7662c3addbeaea3af0fd575
gj <864350301@qq.com>
2020-06-19 14:55:38 +0800

    banner和推荐

---
+++ src/views/category/Category.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>分类</h2>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .wrapper {
+    height: 150px;
+    background-color: red;
+
+    overflow: hidden;
+    /*overflow-y: scroll;*/
+  }
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="home">
+    <nav-bar><div slot="center">购物街</div></nav-bar>
+    <HomeSwiper :banners="banners"></HomeSwiper>
+    <Recommends :recommends="recommends"></Recommends>
+  </div>
+</template>
+
+<script>
+  import NavBar from "../../components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import {getHomeMultidata} from "../../network/home";
+  import Recommends from "./childComps/Recommends";
+
+  export default {
+    name: "Home",
+    components: {
+      Recommends,
+      HomeSwiper,
+      NavBar,
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      this.getHomeMultidata()
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          // this.result = res;
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
